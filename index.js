4bea65bdf8bf175fdb5dbd2ec6eef2a18902f4cd
Carlj0666 <carlj1881@yahoo.com>
2021-04-25 11:40:18 -0700

    added fetch, tested for data access in front end. Working on render pedals function

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const pedals = document.getElementById("pedalsList")
+
 fetch("http://127.0.0.1:3000/pedals")
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => renderPedals(data))
+
+function renderPedals(dataFromFetch) {
+  //get the pedals from data
+  const pedals = dataFromFetch.data
+  //iterate over each pedal in the json array and associate it with an li
+  pedals.forEach(pedal => console.log(pedal))
+  //create li's for the pedals
+  debugger
+
+}
+
