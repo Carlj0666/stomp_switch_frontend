85232239e2659ab564903e065a984465b8248a28
Carlj0666 <carlj1881@yahoo.com>
2021-04-26 12:10:13 -0700

    updated display to show brand name

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ fetch("http://127.0.0.1:3000/pedals")
 .then(response => response.json())
 .then(data => renderPedals(data))
 
+
 function renderPedals(dataFromFetch) {
+  
   //get the pedals from data
   const pedals = dataFromFetch.data
+  console.log(pedals)
   //iterate over each pedal in the json array and associate it with an li
   pedals.forEach(pedal => {
   //create li's for the pedals
     const liTag = document.createElement("li")
     //assign the inner text of the li
-    liTag.innerText = `${pedal.attributes.name}`
+    liTag.innerText = `${pedal.attributes.brand_name}`
     pedalsUl.append(liTag)
-  debugger  
 
   })
 }
+
+// function renderBRand()
+
